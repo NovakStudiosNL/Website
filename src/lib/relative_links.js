d23faa5e98a137ae4b29299d0fba4f30c5c48733
LEO KRAMER~ <leokramer17@gmail.com>
2026-04-05 17:52:41 +0200

    Fix stylesheet link

--- src/lib/relative_links.js
+++ src/lib/relative_links.js
@@ -1,12 +1,18 @@
 const fixRelativeLinks = () => {
 	if (location.hostname == 'localhost' || location.hostname == '127.0.0.1') {
 		document.querySelectorAll('a').forEach((link) => {
-			const href = link.getAttribute('href')
-			if (href && href.includes('/Website')) {
-				link.href = href.replace('/Website', '')
-				console.log(link.getAttribute('href'))
-			}
+			replaceLinkString(link)
 		})
+		document.querySelectorAll('link').forEach((stylesheet) => {
+			replaceLinkString(stylesheet)
+		})
+	}
+}
+
+const replaceLinkString = (el) => {
+	const href = el.getAttribute('href')
+	if (href && href.includes('/Website')) {
+		el.href = href.replace('/Website', '')
 	}
 }
 
