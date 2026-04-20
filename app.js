9bc7dc2f1646f9ed678edafd861ab2294f4dca87
LEO KRAMER~ <leokramer17@gmail.com>
2026-04-20 22:22:09 +0200

    Fix website base path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const BASE_PATH =
 	location.hostname == 'localhost' || location.hostname == '127.0.0.1'
 		? '/'
-		: '/Website'
+		: '/Website/'
 let layoutTemplate
 
 function registerHelpers() {
